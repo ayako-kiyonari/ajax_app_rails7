0a1bfeac0f612bca9d5710b360a63f6b88a1e7fd
ayako-kiyonari <a.kiyonari@gmail.com>
2024-11-29 11:09:11 +0900

    コントローラーにリクエストを送信する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,8 +1,13 @@
 function post (){
- const form = document.getElementById("form");
- form.addEventListener("submit", () => {
-  
- });
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
 };
 
-window.addEventListener('turbo:load',post);
+window.addEventListener('turbo:load', post);
