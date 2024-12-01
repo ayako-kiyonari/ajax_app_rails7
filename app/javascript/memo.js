d8eeb1a626f98eaf6fe3a036afba318a95e7db59
ayako-kiyonari <a.kiyonari@gmail.com>
2024-12-01 12:09:16 +0900

    デプロイのための準備

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,5 @@
 const buildHTML = (XHR) => {
-  const item = XHR.response.post;
+  const item = XHR.response.post;  //レスポンスの中から投稿されたメモの情報をitemに格納
   const html = `
     <div class="post">
       <div class="post-date">
@@ -9,30 +9,34 @@ const buildHTML = (XHR) => {
         ${item.content}
       </div>
     </div>`;
-  return html;
 };
 
 
 function post (){
-  const form = document.getElementById("form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/posts", true);
-    XHR.responseType = "json";
-    XHR.send(formData);
-    XHR.onload = () => {
-      if (XHR.status != 200) {
+
+  //JSからサーバーサイドへリクエストを送信
+  const form = document.getElementById("form");  //index.htmlのid=formから取得してformに代入
+  form.addEventListener("submit", (e) => {  //投稿ボタンがクリックされたらイベント発火
+    e.preventDefault(); //ブラウザとJSのリクエストの重複を防ぐため投稿ボタンのクリックを無効化
+    const formData = new FormData(form);  //内容をサーバー再度に送信するためにフォーム自体の要素を取得
+    const XHR = new XMLHttpRequest(); //JSを用いてサーバーとHTTP通信を行う時に利用
+    XHR.open("POST", "/posts", true);  // open:リクエストを初期化するメソッド
+    XHR.responseType = "json"; //リクエスト送信時にレスポンスでほしいデータフォーマットを指定
+    XHR.send(formData); //send:リクエストを送信するメソッド
+
+    //
+    XHR.onload = () => { //onload: リクエストの送信が成功したときに呼び出される
+      if (XHR.status != 200) { //特定のHTTPリクエストが正常に完了したかどうか
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
       };
       const list = document.getElementById("list");
-      const formText = document.getElementById("content");
-        list.insertAdjacentHTML("afterend",  buildHTML(XHR));
-        formText.value = "";
+      const formText = document.getElementById("content"); //リセット対象となるcontentを取得してformTextに格納
+        list.insertAdjacentHTML("afterend",  buildHTML(XHR)); //htmlをある要素の指定した箇所に挿入する
+        formText.value = ""; //（投稿フォームに前回の入力が残ったのをリセット）
     };
   });
 };
 
 window.addEventListener('turbo:load', post);
+//ページが読み込まれた後にJSのコードを実行するように、loadイベント
